228bfbe1a3dc9f9fc635ea7399816ec0a11fcd0e
Adam Gerber <gerber@cs.uchicago.edu>
2013-12-23 21:38:39 -0600

    remove boilerplate params that we will not use

--- App/src/main/java/com/apress/gerber/EditFragment.java
+++ App/src/main/java/com/apress/gerber/EditFragment.java
@@ -19,14 +19,7 @@ import android.view.ViewGroup;
  *
  */
 public class EditFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -42,8 +35,7 @@ public class EditFragment extends Fragment {
     public static EditFragment newInstance(String param1, String param2) {
         EditFragment fragment = new EditFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,8 +47,7 @@ public class EditFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
